60f1d72bff4b5ddd4ae3f4b53d5b5cb80b556ac4
Luis Felipe Tejada Padilla <lufepa@live.com>
2016-03-06 08:08:53 -0500

    Venv

--- Hotel/settings.py
+++ Hotel/settings.py
@@ -1,5 +1,4 @@
 import psycopg2
-import dj_database_url
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -93,14 +92,15 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = '/static/'
 
-# DATABASES['default'] = dj_database_url.config()
-#
-# SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO","https")
-#
-# ALLOWED_HOSTS = ["*"]
-#
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-#
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR,'static')
-# ]
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO","https")
+
+ALLOWED_HOSTS = ["*"]
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
